4e1c1e198d5f88169e65f24d03365904f1922b73
Rashika Agrawal <rashi3108agrawal@gmail.com>
2026-05-19 22:47:43 +0530

    [139] [Time Beats: 49.63%] [Memory Beats: 55.37%] - LeetPush

--- Word-Break.java
+++ Word-Break.java
@@ -8,13 +8,14 @@
 8        }
 9        dp[0]= true;
 10        for(int i=0;i<=n;i++){
-11            for(int j=i+1;j<=n;j++){
-12                String temp = s.substring(i,j);
-13                if(st.contains(temp)&& dp[i]){
-14                    dp[j] = true;
-15                }
-16            }
-17        }
-18        return dp[n];
-19    }
-20}
+11            if(!dp[i]) continue;
+12            for(int j=i+1;j<=n;j++){
+13                String temp = s.substring(i,j);
+14                if(st.contains(temp)&& dp[i]){
+15                    dp[j] = true;
+16                }
+17            }
+18        }
+19        return dp[n];
+20    }
+21}
