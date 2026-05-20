c5300aed7ee2460ade5147633f66049497efebac
Rashika Agrawal <rashi3108agrawal@gmail.com>
2026-05-20 18:06:59 +0530

    [206] [Time Beats: 100.00%] [Memory Beats: 81.82%] - LeetPush

---
+++ Reverse-Linked-List.java
@@ -0,0 +1,24 @@
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverseList(ListNode head) {
+13        if(head == null || head.next == null) return head;
+14        ListNode prev = null;
+15        ListNode curr = head;
+16        while(curr!= null){
+17            ListNode next = curr.next;
+18            curr.next = prev;
+19            prev= curr;
+20            curr= next;
+21        }
+22        return prev;
+23    }
+24}
