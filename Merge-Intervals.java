75cb52b6e5e6c283d5a864f2dc2ed132921a8676
Rashika Agrawal <rashi3108agrawal@gmail.com>
2026-05-21 17:55:01 +0530

    [56] [Time Beats: 90.92%] [Memory Beats: 13.01%] - LeetPush

--- Merge-Intervals.java
+++ Merge-Intervals.java
@@ -1,23 +1,22 @@
 1class Solution {
 2    public int[][] merge(int[][] intervals) {
-3
-4        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-5
-6        List<int[]> res = new ArrayList<>();
-7
-8        for (int[] interval : intervals) {
-9
-10            // no overlap
-11            if (res.isEmpty() || res.get(res.size() - 1)[1] < interval[0]) {
-12                res.add(interval);
-13            }
-14            // overlap
-15            else {
-16                res.get(res.size() - 1)[1] =
-17                    Math.max(res.get(res.size() - 1)[1], interval[1]);
-18            }
-19        }
-20
-21        return res.toArray(new int[res.size()][]);
-22    }
-23}
+3        int n = intervals.length;
+4        List<int[]> res = new ArrayList<>();
+5        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+6        int lstSt= intervals[0][0];
+7        int lstEnd = intervals[0][1];
+8
+9        for(int i=1;i<n;i++){
+10            if(intervals[i][0]<=lstEnd){
+11                lstSt = Math.min(lstSt, intervals[i][0]);
+12                lstEnd = Math.max(lstEnd, intervals[i][1]);
+13            }else{
+14                res.add(new int[]{lstSt, lstEnd});
+15                lstSt = intervals[i][0];
+16                lstEnd = intervals[i][1];
+17            }
+18        }
+19        res.add(new int[]{lstSt, lstEnd});
+20        return res.toArray(new int[res.size()][]);
+21    }
+22}
